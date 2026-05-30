34fb1d1a2834dcd3116464ff79c5857f12f064f0
Muhammad Musawar Ali Shah <Musawaralishah8@gmail.com>
2026-05-31 01:53:35 +0500

    Fix DATABASE_URL to replace 'postgres://' with 'postgresql://'

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 import psycopg2.extras
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL", "").replace("postgres://", "postgresql://")
 
 
 def get_connection():
