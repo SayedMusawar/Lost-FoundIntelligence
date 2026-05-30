fe9fc7bb76386339e129cb7cada9b38474310534
Muhammad Musawar Ali Shah <Musawaralishah8@gmail.com>
2026-05-31 01:50:54 +0500

    Change CORS settings to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -9,10 +9,7 @@ app = FastAPI(title="FAST Lost & Found API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://lost-found-intelligence.vercel.app",
-        "https://lost-found-intelligence-1e93b94q7-p240619-9725s-projects.vercel.app",
-        "http://localhost:5173",
-    ],
+        "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
